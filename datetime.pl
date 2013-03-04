4548c259aebdddfc8b5c91d8be2546c2332bd220
H Waldo G <gwaldo@gmail.com>
2013-03-04 11:44:09 -0500

    adding more thorough and flexible possiblity to perl datetime

--- datetime.pl
+++ datetime.pl
@@ -1,12 +1,31 @@
 #! /usr/bin/env perl
 
-use Win32::OLE;
-use POSIX qw(strftime);
+use strict;
+use warnings;
 
-print "The time is " . fnDateTime() .".  Have a nice day\n\n";
-print "blah, blah, blah\n" . "yackety schmakety\n";
+use DateTime;
 
-sub fnDateTime
-{
-	strftime "%Y%m%d_%H%M%S", localtime;
-}
+my $today = DateTime->now( time_zone => 'local' );
+
+my $timeOfDay		= $today->hour() . $today->minute();
+my $yy_today		= $today->strftime( '%y%m%d' );
+my $yyyy_today		= $today->ymd('');
+my $yy_yesterday	= DateTime->now->subtract( days => 1)->strftime( '%y%m%d' );
+my $yyyy_yesterday	= DateTime->now->subtract( days => 1)->strftime( '%Y%m%d' );
+
+my $datetime		= $yyyy_today . "_" . $today->hour() . $today->minute() . $today->second();
+
+print $datetime . "\n";
+
+# # OR...
+#
+# use POSIX qw(strftime);
+#
+# print "The time is " . fnDateTime() .".  Have a nice day\n\n";
+#
+# sub fnDateTime
+# {
+	# strftime "%Y%m%d_%H%M%S", localtime;
+# }
+
+
